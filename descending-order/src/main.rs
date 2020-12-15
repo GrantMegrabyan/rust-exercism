ac59916f213b46d0a4c03dcf0f113c7e45d1cdd7
Grant Megrabyan <grant.megrabyan@gmail.com>
2020-12-15 23:07:14 +0000

    Integer in descending order

---
+++ descending-order/src/main.rs
@@ -0,0 +1,6 @@
+mod sorter;
+
+fn main() {
+    println!("{0} -> {1}", 412532, sorter::descending_order(412532));
+}
+
---
+++ descending-order/src/sorter.rs
@@ -0,0 +1,32 @@
+pub fn descending_order(x: u64) -> u64 {
+    let mut v: Vec<u64> = vec![];
+    let mut tmp = x;
+    while tmp > 0 {
+        let r = tmp % 10;
+        tmp = tmp / 10;
+        v.push(r);
+    }
+    v.sort();
+    v.reverse();
+    let mut ordered = 0;
+    for digit in v {
+        ordered = ordered * 10 + digit;
+    }
+    ordered
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests() {
+        assert_eq!(descending_order(0), 0);
+        assert_eq!(descending_order(1), 1);
+        assert_eq!(descending_order(15), 51);
+        assert_eq!(descending_order(1021), 2110);
+        assert_eq!(descending_order(123456789), 987654321);
+        assert_eq!(descending_order(145263), 654321);
+        assert_eq!(descending_order(1254859723), 9875543221);
+    }
+}
