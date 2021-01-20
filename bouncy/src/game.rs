d5b0b77dab884d9f8a86979145831e6109302386
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-01-20 00:31:44 +0000

    Handle Err results in Display impl of Game struct

--- bouncy/src/game.rs
+++ bouncy/src/game.rs
@@ -27,29 +27,29 @@ impl Game {
 
 impl Display for Game {
     fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        let horiz_border = |fmt: &mut Formatter| {
-            write!(fmt, "+");
+        let horiz_border = |fmt: &mut Formatter| -> std::fmt::Result {
+            write!(fmt, "+")?;
             for _ in 0..self.frame.width {
-                write!(fmt, "-");
+                write!(fmt, "-")?;
             }
             write!(fmt, "+\n")
         };
 
-        let row = |fmt: &mut Formatter, ball: &Ball, row_num: u32| {
-            write!(fmt, "|");
+        let row = |fmt: &mut Formatter, ball: &Ball, row_num: u32| -> std::fmt::Result {
+            write!(fmt, "|")?;
             for col_num in 0..self.frame.width {
                 if row_num == ball.y && col_num == ball.x {
-                    write!(fmt, "o");
+                    write!(fmt, "o")?;
                 } else {
-                    write!(fmt, " ");
+                    write!(fmt, " ")?;
                 }
             }
-            write!(fmt, "|\n");
+            write!(fmt, "|\n")
         };
 
         horiz_border(fmt)?;
         for row_num in 0..self.frame.height {
-            row(fmt, &self.ball, row_num);
+            row(fmt, &self.ball, row_num)?;
         }
         horiz_border(fmt)
     }
