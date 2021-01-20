e1ea8fa327518e136e9446972cecf7fb7806eb7f
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-01-20 00:40:04 +0000

    Return error from main in case of ParseError

--- bouncy/src/main.rs
+++ bouncy/src/main.rs
@@ -5,19 +5,15 @@ mod types;
 
 use parse_args::parse_args;
 
-fn main() {
-    match parse_args() {
-        Err(err) => eprintln!("{:?}", err),
-        Ok(frame) => {
-            let mut game = game::Game::new(frame);
-            let sleep_duration = std::time::Duration::from_millis(33);
-            loop {
-                // Clear screen
-                print!("\x1B[2J\x1B[1;1H");
-                println!("{}", game);
-                game.step();
-                std::thread::sleep(sleep_duration);
-            }
-        }
+fn main() -> Result<(), types::ParseError> {
+    let frame = parse_args()?;
+    let mut game = game::Game::new(frame);
+    let sleep_duration = std::time::Duration::from_millis(33);
+    loop {
+        // Clear screen
+        print!("\x1B[2J\x1B[1;1H");
+        println!("{}", game);
+        game.step();
+        std::thread::sleep(sleep_duration);
     }
 }
